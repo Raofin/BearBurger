debea491bffe2095edf6f045308bb0ee90e8244a
Raofin <raofin@hotmail.com>
2022-08-31 02:36:08 +0600

    Update sql file

--- src/main/resources/app.sql
+++ src/main/resources/app.sql
@@ -1,10 +1,10 @@
-DROP DATABASE IF EXISTS Bearburger_Jsp;
+DROP DATABASE IF EXISTS BearBurger_JSP;
 
 # create database
 CREATE DATABASE IF NOT EXISTS BearBurger_JSP;
 
 # select the database
-USE Bearburger_Jsp;
+USE BearBurger_JSP;
 
 # create users table
 CREATE TABLE IF NOT EXISTS Users
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS Users
     UserID      INT AUTO_INCREMENT PRIMARY KEY,
     Username    VARCHAR(30) NOT NULL UNIQUE,
     Email       VARCHAR(30) NOT NULL,
-    Password    VARCHAR(30) NOT NULL,
+    Password    VARCHAR(62) NOT NULL,
     PhoneNumber VARCHAR(15) NOT NULL,
     Gender      VARCHAR(6)  NOT NULL,
     Spent       INT,
@@ -34,18 +34,18 @@ CREATE TABLE IF NOT EXISTS Roles
 
 # insert user data
 INSERT IGNORE INTO Users (Username, Email, Password, PhoneNumber, Gender, Spent, Enabled)
-VALUES ('Raofin', 'hello@raofin.net', 'Asdfgh', '+8801234567890', 'male', 6801, TRUE),
-       ('Sazzad', 'sazzad96@gmail.com', 'sazzaz123', '+1263343299123', 'male', 9252, TRUE),
-       ('Authoi', 'authoi123@gmail.com', 'authoi999', '+2369543156441', 'female', 8056, TRUE),
-       ('Bill Gates', 'billgates@outlook.com', 'billgates68457', '+6963343233159', 'male', 9960, TRUE),
-       ('Elon Musk', 'elonmusk@yahoo.com', 'elon123', '+9668508170248', 'male', 7856, FALSE),
-       ('Jack Ma', 'jackma@gmail.com', 'jackma144', '+1667698473784', 'male', 4567, TRUE),
-       ('Steve Jobs', 'stevejobs@icloud.com', 'steve1213', '+1527475095845', 'male', 421, TRUE),
-       ('Jeff Bezos', 'jeffbezos@gmail.com', 'jeffbe1334', '+8966295324845', 'male', 2152, FALSE),
-       ('Mark Zuckerberg', 'markzuckerberg@live.com', 'markz131', '+2657146731697', 'male', 3972, TRUE),
-       ('Sundar Pichai', 'sundarpichai@gmail.com', 'sundarp296', '+9815680737969', 'male', 1546, FALSE),
-       ('1111', '1111@1111.com', '1111', '+4795131456789', 'male', 0, TRUE),
-       ('admin', 'admin@email.com', 'admin', '+6478912356147', 'male', 0, TRUE);
+VALUES ('Raofin', 'hello@raofin.net', '$2a$10$l1xCEl5Vns6NbmzeiRPWpuy7nWB9ikSI/A6z8SipFQoOKL8HojO.m', '+8801234567890', 'Male', 6801, TRUE),
+       ('Sazzad', 'sazzad96@gmail.com', '$2a$10$BZ1Ai3VlwUamZg6c9zC8B.hlJzcXJGP512aAxtg2OAt2DT0tSjtPq', '+1263343299123', 'Male', 9252, TRUE),
+       ('Authoi', 'authoi123@gmail.com', '$2a$10$dJGfQ0UJ/TFL7qutdyax8ejxoXJEWsRUDKXxL48dIkVNW9N5rYsRe', '+2369543156441', 'Female', 8056, TRUE),
+       ('Bill Gates', 'billgates@outlook.com', '$2a$10$06Q7zDE.jtKiBsOFOCCileeVHhCaVPi8JMx6zaWWcj3E/JXdi17xy', '+6963343233159', 'Male', 9960, TRUE),
+       ('Elon Musk', 'elonmusk@yahoo.com', '$2a$10$nwkSfj5g0.BZ8kJFIx99jOk9uIcMC.i9S2NRCrmyuR94CmlcezLqW', '+9668508170248', 'Male', 7856, FALSE),
+       ('Jack Ma', 'jackma@gmail.com', '$2a$10$P9cJSj0euTBDp2kiBLzedep7LPjeon4G6pUFOrvy5P7xu9dKaMyUu', '+1667698473784', 'Male', 4567, TRUE),
+       ('Steve Jobs', 'stevejobs@icloud.com', '$2a$10$drTpyeucMtB62LDVyVe8IOONRmd9gFkxSBWioAoFBeT6QPAU5uI06', '+1527475095845', 'Male', 421, FALSE),
+       ('Jeff Bezos', 'jeffbezos@gmail.com', '$2a$10$a3EMVJdkZEVerGKgXK7LuOiU0ck5cH.iD5g3Xz/nMUnGmAVjjyNC.', '+8966295324845', 'Male', 2152, TRUE),
+       ('Mark Zuckerberg', 'markzuckerberg@live.com', '$2a$10$GCaVUadVfLdrZEE7jn6iWu2bMpHpXn.X3DJSIqILslJNGvXZsynoq', '+2657146731697', 'Male', 3972, TRUE),
+       ('Sundar Pichai', 'sundarpichai@gmail.com', '$2a$10$qojQDSjU7o.VOE4XmjfIxOanI4kFK3UmN9hoaxZPD.PVbOOCk/nRC', '+9815680737969', 'Male', 1546, FALSE),
+       ('1111', '1111@1111.com', '$2a$10$w9byGF9YAD0U29wQdNpICehY.nBHBxZ47J4tTO8Mq46h9OFI2iMMO', '+4795131456789', 'Male', 68, TRUE),
+       ('admin', 'admin@email.com', '$2a$10$3l0p7n2pIIykRYaPsPbvt.8y60kvyNF9E7Q6e21sMi7tBRPqL8zvS', '+6478912356147', 'Male', 0, TRUE);
 
 # insert user data
 INSERT IGNORE INTO Roles (UserID, Role)
@@ -73,27 +73,12 @@ CREATE TABLE IF NOT EXISTS Foods
     FoodID      INT AUTO_INCREMENT PRIMARY KEY,
     Category    VARCHAR(30) NOT NULL,
     Title       VARCHAR(30) NOT NULL UNIQUE,
-    DESCRIPTION TEXT        NOT NULL,
+    Description TEXT        NOT NULL,
     Price       INT         NOT NULL
 );
 
-# create users table
-CREATE TABLE IF NOT EXISTS Comments
-(
-    CommentID INT AUTO_INCREMENT PRIMARY KEY,
-    ParentID  INT         NOT NULL,
-    FoodID    INT         NOT NULL,
-    PostedBy  VARCHAR(30) NOT NULL,
-    Comment   TEXT        NOT NULL,
-    PostDate  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    #     CONSTRAINT FoodID_FK
-    #         FOREIGN KEY (FoodID)
-    #             REFERENCES Foods (FoodID)
-    #             ON DELETE CASCADE
-);
-
 # insert food data
-INSERT IGNORE INTO Foods (Category, Title, DESCRIPTION, Price)
+INSERT IGNORE INTO Foods (Category, Title, Description, Price)
 VALUES ('Burger', 'Cheese Burger', 'Prepared with beef patty, cheese, burger sauce, pickles & onion', 650),
        ('Burger', 'Bacon Cheese Burger', 'Prepared with beef patty, 2 slices cheese, bacon & burger sauce', 500),
        ('Burger', 'Double Cheese Burger', 'Prepared with 2 beef patties, double cheese, burger sauce & onion', 640),
@@ -134,13 +119,3 @@ VALUES ('Burger', 'Cheese Burger', 'Prepared with beef patty, cheese, burger sau
        ('Sides', 'Large French Fry', 'Delicious french fry in large', 110),
        ('Sides', 'Chicken Fingers', 'Chicken fried in finger sized', 130),
        ('Sides', 'Naga Drumsticks', 'You like it HOT!! Its for you', 120);
-
-# insert comment data
-INSERT IGNORE INTO Comments (CommentID, ParentID, FoodID, PostedBy, Comment, PostDate)
-VALUES (1, 0, 1, 'Raofin', 'I have to say, I enjoyed every single bite of the meal.', '2022-07-17 22:27:04'),
-       (2, 1, 1, 'Bill Gates', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum maxime modi necessitatibus rem sed. Ad consequuntur dolorem nobis sequi tempora?', '2022-07-17 22:28:28'),
-       (3, 2, 1, 'Raofin', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, unde!', '2022-07-17 22:29:36'),
-       (4, 2, 1, 'Bill Gates', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', '2022-07-17 22:30:55'),
-       (5, 0, 1, 'Elon Musk', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, sequi.', '2022-07-17 22:31:48'),
-       (6, 4, 1, 'Steve Jobs', 'Lorem ipsum dolor sit amet, consectetur.', '2022-07-17 22:32:46'),
-       (7, 0, 1, 'Jeff Bezos', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores cumque dolore, harum iure necessitatibus perferendis quasi sequi suscipit tempore? Aperiam culpa delectus ducimus inventore nam possimus praesentium provident quaerat quas, quidem quisquam, recusandae sunt suscipit totam ullam vitae, voluptatibus.', '2022-07-17 22:35:34');
