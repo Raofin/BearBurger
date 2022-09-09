5efd3d222964e289389f4800c49edc2dd59224b2
Raofin <raofin@hotmail.com>
2022-09-09 17:12:46 +0600

    Init spring boot

---
+++ src/main/java/net/raofin/bearburger/BearBurgerApplication.java
@@ -0,0 +1,13 @@
+package net.raofin.bearburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearBurgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearBurgerApplication.class, args);
+    }
+
+}
--- src/main/java/net/raofin/bearburger/config/AppSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package net.raofin.bearburger.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class AppSecurityInitializer extends AbstractSecurityWebApplicationInitializer
-{
-}
